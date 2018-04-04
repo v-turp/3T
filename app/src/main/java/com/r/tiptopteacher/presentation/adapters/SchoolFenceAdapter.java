259cd171c6acb7b1d34b71564f11df1605750ca3
victory <vturp@yahoo.com>
2018-04-04 09:57:18 -0700

    Added the view holder for the school search grid. The names variable names are temporary. This is just for placement and to get it to work. I want to see what the gridlayout looks like with this setup.

--- app/src/main/java/com/r/tiptopteacher/presentation/adapters/SchoolFenceAdapter.java
+++ app/src/main/java/com/r/tiptopteacher/presentation/adapters/SchoolFenceAdapter.java
@@ -2,25 +2,29 @@ package com.r.tiptopteacher.presentation.adapters;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.r.tiptopteacher.R;
 
 /**
  * This is the adapter for the list of schools that will
  * be displayed below the map.
  */
 
-public class SchoolFenceAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class SchoolFenceAdapter extends RecyclerView.Adapter<SchoolFenceAdapter.SchoolFenceViewHolder> {
 
     public SchoolFenceAdapter(){}
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public SchoolFenceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull SchoolFenceViewHolder holder, int position) {
 
     }
 
@@ -29,9 +33,21 @@ public class SchoolFenceAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         return 0;
     }
 
-    public static class SchoolFenceViewHolder{
+    public static class SchoolFenceViewHolder extends RecyclerView.ViewHolder{
 
+        public TextView col1;
+        public TextView col2;
+        public TextView col3;
+        public TextView col4;
 
+        public SchoolFenceViewHolder(View itemView) {
+            super(itemView);
+
+            col1 = (TextView) itemView.findViewById(R.id.tvCol1);
+            col2 = (TextView) itemView.findViewById(R.id.tvCol2);
+            col3 = (TextView) itemView.findViewById(R.id.tvCol3);
+            col4 = (TextView) itemView.findViewById(R.id.tvCol4);
+        }
     }
 
 
